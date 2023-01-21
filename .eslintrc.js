ba5d838fe4da55f0845fb429c303d417a67b03da
AlexeyChernov88 <chernoff.aleksey@gmail.com>
2023-01-22 02:17:41 +0300

    thrird

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,30 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ["plugin:react/recommended", "standard", "prettier"],
+    extends: ["plugin:react/recommended", "standard"],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
         },
-        ecmaVersion: "latest",
+        ecmaVersion: 12,
         sourceType: "module"
     },
     plugins: ["react"],
     rules: {
-        // indent: ["error", 4],
-        // semi: [2, "always"],
-        // "space-before-function-paren": [
-        //     "error",
-        //     { anonymous: "always", named: "never" }
-        // ],
-        // "multiline-ternary": [off],
-        // quotes: ["error", "double", { allowTemplateLiterals: true }]
+        semi: [2, "always"],
+        indent: [0, 4],
+        "space-before-function-paren": [
+            "error",
+            { anonymous: "always", named: "never" }
+        ],
+        "multiline-ternary": ["off"],
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true,
+                avoidEscape: true
+            }
+        ]
     }
 };
